70f111418718828eb32b4cc40a8ff07a6a5f546a
TheDragonRing <thedragonring.bod@gmail.com>
2017-04-10 22:11:24 +1000

    Update new-template progress

--- new-template/assets/main.js
+++ new-template/assets/main.js
@@ -11,13 +11,20 @@ $(document).ready(function(){
   .hide()
   .fadeIn(1500);
 
+  //Remove underline on anchor elements that have a child element.
+  $('a').each(function(){
+    if($(this).children().length > 0){
+      $(this).css('border', 'none');
+    }
+  });
+
   //Manage page changes.
   function changePage(){
     $('article').each(function(){
       //Check if another page is being accessed.
       if($(this).attr('id') === /[^\#]*$/.exec(window.location.href)[0] && $(this).css('display') !== 'block'){
         //Set to requested page.
-        $('#home').hide(1000);
+        $('#home').fadeOut(1000);
         $(this).css('display', 'block')
         .show()
         .hide()
@@ -26,7 +33,7 @@ $(document).ready(function(){
       //Set back to the home page.
       if(/[^\#]*$/.exec(window.location.href)[0] === ''){
         $('#home').fadeIn(1500);
-        $('article').hide(1000);
+        $('article').fadeOut(1000);
       }
     });
   }
@@ -34,6 +41,18 @@ $(document).ready(function(){
   setInterval(function(){
     changePage();
   }, 1);
+
+  //Read page GET requests.
+  function getRequests(){
+    var s1 = location.search.substring(1, location.search.length).split('&'), //Separate data.
+    r = {}, s2, i;
+    for (i = 0; i < s1.length; i += 1) {
+      s2 = s1[i].split('='); //Separate paramenters.
+      r[decodeURIComponent(s2[0]).toLowerCase()] = decodeURIComponent(s2[1]); //Store data in variable.
+    }
+    return r;
+  };
+  var QueryString = getRequests(); //Usage: If the url is "index.html?q=test" then "QueryString['q']" will return "test".
 
   //Set the year in the footer to either the current year or the specified year till the current year.
   function getYear(){

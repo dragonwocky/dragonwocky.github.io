2a22d8089439625363f86f3080702edf94e80c65
dragonwocky <thedragonring.bod@gmail.com>
2020-04-02 21:54:19 +1100

    make cards clickable

--- wip/build.js
+++ wip/build.js
@@ -27,44 +27,46 @@ data.posts = data.posts || [];
     entry => `
         <section style="border-top-color: ${entry.colour};">
           <a href="${entry.url}" class="card-link"></a>
-          <h3>${entry.name}</h3>
-          <p>${entry.desc}</p>
-          ${
-            entry.badges
-              ? '<ul>' +
-                (entry.badges.github
-                  ? `<li class="badge" style="--badge: #b7c0cbb4" ${
-                      entry.badges.npm
-                        ? ''
-                        : `data-githubV="${entry.badges.github}"`
-                    }>
-                      <a href="https://github.com/${entry.badges.github}">
-                        <b><img
-                          src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
-                        />github</b>
-                      </a>
-                    </li>`
-                  : '') +
-                (entry.badges.npm
-                  ? `<li class="badge" style="--badge: #c12127" data-npmV="${entry.badges.npm}">
-                      <a href="https://www.npmjs.com/package/${entry.badges.npm}">
-                        <b><img
-                            src="https://raw.githubusercontent.com/npm/logos/master/npm%20logo/npm-logo-red.png"
-                            style="width: 1.25em; height: auto"
-                          />npm</b>
-                      </a>
-                    </li>`
-                  : '') +
-                (entry.badges.license.length
-                  ? `<li class="badge" style="--badge: ${entry.colour}">
-                      <a href="${entry.badges.license[1]}">
-                        <b>license</b><span>${entry.badges.license[0]}</span>
-                      </a>
-                    </li>`
-                  : '') +
-                '</ul>'
-              : ''
-          }
+          <div class="card-content">
+            <h3>${entry.name}</h3>
+            <p>${entry.desc}</p>
+            ${
+              entry.badges
+                ? '<ul>' +
+                  (entry.badges.github
+                    ? `<li class="badge" style="--badge: #b7c0cbb4" ${
+                        entry.badges.npm
+                          ? ''
+                          : `data-githubV="${entry.badges.github}"`
+                      }>
+                        <a href="https://github.com/${entry.badges.github}">
+                          <b><img
+                            src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+                          />github</b>
+                        </a>
+                      </li>`
+                    : '') +
+                  (entry.badges.npm
+                    ? `<li class="badge" style="--badge: #c12127" data-npmV="${entry.badges.npm}">
+                        <a href="https://www.npmjs.com/package/${entry.badges.npm}">
+                          <b><img
+                              src="https://raw.githubusercontent.com/npm/logos/master/npm%20logo/npm-logo-red.png"
+                              style="width: 1.25em; height: auto"
+                            />npm</b>
+                        </a>
+                      </li>`
+                    : '') +
+                  (entry.badges.license.length
+                    ? `<li class="badge" style="--badge: ${entry.colour}">
+                        <a href="${entry.badges.license[1]}">
+                          <b>license</b><span>${entry.badges.license[0]}</span>
+                        </a>
+                      </li>`
+                    : '') +
+                  '</ul>'
+                : ''
+            }
+          </div>
         </section>`
   );
   fsp.writeFile(
